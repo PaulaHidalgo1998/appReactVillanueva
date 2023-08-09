8ad703aedec29969a8c510b39d4827bf63303769
Adrián Ruiz <93943507+AdrianHouseholder@users.noreply.github.com>
2023-08-09 16:27:15 +0200

    Update getPersonas.js

--- pages/api/getPersonas.js
+++ pages/api/getPersonas.js
@@ -1,14 +1,13 @@
 import fs from 'fs/promises'; // Using the built-in promises version of the fs module
+import path from 'path';
 
 const file_path = 'database.json';
 
 export default async function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     if (req.method === 'GET') {
         try {
-            const content = await fs.readFile(file_path, 'utf-8');
+            const full_path = path.join(process.cwd(), file_path);
+            const content = await fs.readFile(full_path, 'utf-8');
             res.status(200).json({content});
         } catch (error) {
             res.status(500).json({error: 'Error reading file'});
